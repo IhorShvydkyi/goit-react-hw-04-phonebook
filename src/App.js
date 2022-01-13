92d320fc0b36ce0fa2cef67f50f296e8ef6f7855
IhorShvydkyi <igorshvydky35@gmail.com>
2022-01-13 22:14:30 +0200

    form submit in app

--- src/App.js
+++ src/App.js
@@ -1,80 +1,72 @@
-import { Component } from "react";
+import { useState } from "react";
 import { Container } from "./App.styled";
 import Form from "./components/Form/Form";
 import Filter from "./components/Filter/Filter";
 import ContactList from "./components/Contacts/ContactList";
 
-class App extends Component {
-  state = {
-    contacts: [],
-    filter: "",
-  };
+export default function App() {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState("");
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
-    }
-  }
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (this.state.contacts !== prevState.contacts) {
+  //     localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+  //   }
+  // }
 
-  componentDidMount() {
-    const contacts = localStorage.getItem("contacts");
-    const parsedContacts = JSON.parse(contacts);
+  // componentDidMount() {
+  //   const contacts = localStorage.getItem("contacts");
+  //   const parsedContacts = JSON.parse(contacts);
 
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
+  //   if (parsedContacts) {
+  //     this.setState({ contacts: parsedContacts });
+  //   }
+  // }
 
-  onSubmitContactForm = (data) => {
-    if (this.state.contacts.find((contact) => contact.name === data.name)) {
+  const onSubmitContactForm = (data) => {
+    if (contacts.find((contact) => contact.name === data.name)) {
       alert(`${data.name} is already in contacts.`);
     } else {
-      this.setState(({ contacts }) => ({
-        contacts: [data, ...contacts],
-      }));
+      setContacts((prevContacts) => [data, ...prevContacts]);
     }
   };
 
-  handleContactsFilter = (e) => {
-    this.setState({ filter: e.currentTarget.value });
-  };
+  // handleContactsFilter = (e) => {
+  //   this.setState({ filter: e.currentTarget.value });
+  // };
 
-  getVisibleContacts = () => {
-    const { filter, contacts } = this.state;
-    const filterNormalize = filter.toLowerCase();
-    return contacts.filter((contact) => {
-      return contact.name.toLowerCase().includes(filterNormalize);
-    });
-  };
+  // getVisibleContacts = () => {
+  //   const { filter, contacts } = this.state;
+  //   const filterNormalize = filter.toLowerCase();
+  //   return contacts.filter((contact) => {
+  //     return contact.name.toLowerCase().includes(filterNormalize);
+  //   });
+  // };
 
-  onDeleteContact = (contactId) => {
-    this.setState((prevState) => ({
-      contacts: prevState.contacts.filter((contact) => {
-        return contact.id !== contactId;
-      }),
-    }));
-  };
+  // onDeleteContact = (contactId) => {
+  //   this.setState((prevState) => ({
+  //     contacts: prevState.contacts.filter((contact) => {
+  //       return contact.id !== contactId;
+  //     }),
+  //   }));
+  // };
 
-  render() {
-    const { filter } = this.state;
-    const visibleContacts = this.getVisibleContacts();
-    return (
-      <Container>
-        <h1>Phonebook</h1>
-        <Form onSubmit={this.onSubmitContactForm} />
-        <h2>Contacts</h2>
-        <Filter onChange={this.handleContactsFilter} value={filter} />
-        {visibleContacts.length === 0 && filter.length > 0 ? (
-          <p>No results for your search</p>
-        ) : (
-          <ContactList
-            contacts={visibleContacts}
-            onDeleteContact={this.onDeleteContact}
-          />
-        )}
-      </Container>
-    );
-  }
+  // const { filter } = this.state;
+  // const visibleContacts = this.getVisibleContacts();
+  return (
+    <Container>
+      <h1>Phonebook</h1>
+      <Form onSubmit={onSubmitContactForm} />
+      {/* <h2>Contacts</h2>
+			<Filter onChange={this.handleContactsFilter} value={filter} />
+			{visibleContacts.length === 0 && filter.length > 0 ? (
+				<p>No results for your search</p>
+			) : (
+				<ContactList
+					contacts={visibleContacts}
+					onDeleteContact={this.onDeleteContact}
+				/>
+			)} */}
+    </Container>
+  );
 }
-
-export default App;
